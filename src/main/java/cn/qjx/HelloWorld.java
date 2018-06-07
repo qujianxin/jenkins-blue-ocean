8e4663eaa6df27d268c0dfa1875a6e7550026151
Jianxin Qu <jianxin.qu@ericsson.com>
2018-06-07 11:14:02 +0800

    first commit

---
+++ src/main/java/cn/qjx/HelloWorld.java
@@ -0,0 +1,10 @@
+package cn.qjx;
+
+/**
+ * Created by ejinquu on 6/7/2018.
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello world");
+    }
+}
